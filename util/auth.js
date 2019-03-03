cde00e98c4fc908a698b7e95ab72208ca4c21f20
alex-taxiera <alex.taxiera@gmail.com>
2019-03-02 19:29:32 -0500

    make auth easier

--- util/auth.js
+++ util/auth.js
@@ -1,8 +1,15 @@
-function auth(myKey, givenKey) {
-    if (myKey !== null && myKey && myKey !== undefined && givenKey !== myKey) {
-        return false
-    } else {
+function auth(keys) {
+  keys = keys.filter((key) => key)
+  return (pass) => {
+    if (keys && keys.length > 0) {
+      if (pass && keys.some((key) => pass === key)) {
         return true
+      } else {
+        return false
+      }
+    } else {
+      return true
     }
+  }
 }
 module.exports = auth
